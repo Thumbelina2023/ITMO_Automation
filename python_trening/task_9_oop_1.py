1bcdd2a1082f1d038ceaa46f83c4b0f33669cac4
OLGA <coololga@rambler.ru>
2023-07-06 22:29:41 +0300

    task_9

--- python_trening/task_9_oop_1.py
+++ python_trening/task_9_oop_1.py
@@ -1,5 +1,7 @@
-class Input:
+from task_9_checks import Checks
+class Input(Checks):
     def __init__(self, loc, text):
+        super().__init__(loc)
         self.loc = loc
         self.text = text
 search = Input('input#search', 'Найти')
@@ -7,18 +9,29 @@ catalog = Input('Каталог', 'Текст')
 
 print(search.loc)
 print(catalog.text)
+print(search.check_text())
+print('\n')
 
-class Button:
+class Button(Checks):
     def __init__(self, text, loc):
+        super().__init__(loc)
         self.loc = loc
         self.text = text
 exit = Button('Выход', 'button#exit')
 find = Button('Ввод', 'button#find')
+print(exit.loc)
+print(find.text)
+print(exit.check_text())
+print('\n')
 
-class Title:
+class Title(Checks):
     def __init__(self, text, loc):
+        super().__init__(loc)
         self.loc = loc
         self.text = text
 zagolovok = Title('', 'title#zagolovok')
 a= Title('Welcome', 'title#a')
 
+print(zagolovok.loc)
+print(a.text)
+print(a.check_text())
